58334556f783973a4615f6c81b05b05103d38a36
PetoAdam <peto.adam@yahoo.com>
2023-01-07 01:46:11 +0100

    python - send temperature data to REST api

--- TemperatureQuery/rasp.py
+++ TemperatureQuery/rasp.py
@@ -1,11 +1,25 @@
 import serial
+import requests
 
 # Open the serial port
 with serial.Serial("/dev/ttyACM0", 9600) as ser:
     print("Connected to: " + ser.port)
 
-    # Read data from the serial port
+    # Read data from the serial port and send it to the API
     while True:
         data = ser.readline()
-        temp = data.decode().rstrip()
+        temp = int(data.decode())
         print(temp)
+
+        # Set the content type to application/json
+        headers = { "Content-Type": "application/json" }
+
+        # Construct the POST data as a dictionary
+        post_data = {
+            "Value": temp,
+            "LocationId": 1
+        }
+
+        # Send the POST request to the API
+        response = requests.post("http://89.133.35.34:5000/api/temperatures", json=post_data, headers=headers)
+        print(response.status_code)
